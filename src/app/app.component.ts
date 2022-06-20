04060a85df23b29cd220a78dbad5ecc230dbe56d
Mounir Bkr <miirbri1@gmail.com>
2022-06-21 00:46:21 +0100

    commit2 "routing"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {CatalogueService} from "./catalogue.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,11 @@ export class AppComponent implements OnInit {
   title = 'ecommerce-v1-Angular';
 
   public categories:any ;
+  currentCategorie:any;
 
-  constructor(private catalogueService:CatalogueService){}
+
+  constructor(private catalogueService:CatalogueService,
+              private router:Router){}
 
   ngOnInit(): void {
     this.getCategories();
@@ -23,5 +27,16 @@ export class AppComponent implements OnInit {
         this.categories = data;
         },
       error=>{console.log(error)});
+  }
+
+  getProductByCategorie(c: any) {
+    this.currentCategorie==c;
+    this.router.navigateByUrl('/products/2/'+c.id)
+  }
+
+
+  onSelectedProducts() {
+    this.currentCategorie=undefined;
+    this.router.navigateByUrl("/products/1/0");
   }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {CatalogueService} from "../catalogue.service";
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-products',
@@ -9,14 +10,45 @@ import {CatalogueService} from "../catalogue.service";
 export class ProductsComponent implements OnInit {
   public products: any;
 
-  constructor(public catalogueService:CatalogueService) { }
+
+  constructor(public catalogueService:CatalogueService,
+              private activatedRoute:ActivatedRoute,
+              public router:Router) {}
+
 
   ngOnInit(): void {
-    this.getProducts();
+//IMPORTANT:changement de route sur meme url(..//products/2/2 ,../products/2/3)
+    this.router.events.subscribe((val)=> {
+      //une fois fin de navigation
+      if (val instanceof NavigationEnd) {
+        let url = val.url;
+        console.log(url);
+        let p1 = this.activatedRoute.snapshot.params['p1'];
+
+        if (p1 == 1) {
+          //recuperer les produits selectionés==wich selected=true
+          this.getProducts("/products/search/selectedproducts");
+        } else if (p1 == 2) {
+          //chercher a afficher les produit par categories
+          let idCategorie = this.activatedRoute.snapshot.params['p2'];
+          //donne les produits de la categorie 1 => spring data rest
+          this.getProducts('/categories/' + idCategorie + '/products');
+        }
+        }
+      });
+    let p1 = this.activatedRoute.snapshot.params['p1'];
+    if (p1 == 1) {
+      //recuperer les produits selectionés==wich selected=true
+      this.getProducts("/products/search/selectedproducts");
+    }
   }
 
-  private getProducts() {
-    this.catalogueService.getRessources("/products/search/selectedproducts")
+
+
+
+
+  private getProducts(url:string) {
+    this.catalogueService.getRessources(url)
       .subscribe(data => {
         this.products=data;
       },error=>{
